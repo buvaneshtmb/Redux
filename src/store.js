81b02e15d902db440b93585c596142c28a598ec4
buvaneshtmb <buvaneshtmb@gmail.com>
2023-01-22 00:10:36 +0530

    redux todo and counter

---
+++ src/store.js
@@ -0,0 +1,11 @@
+import {configureStore} from '@reduxjs/toolkit'
+import counterReducer from './counterSlice'
+import todoReducer from './todoSlice'
+
+export default configureStore({
+    reducer:{
+        counter:counterReducer,
+        todo:todoReducer
+
+    }
+})
